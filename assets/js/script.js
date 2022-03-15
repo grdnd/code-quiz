c1bd6139e40a8edbb317f819dd2cbe2a9a95b0e4
Gordo <gordonhdo@gmail.com>
2022-03-15 07:13:55 -0700

    Updated comments | finally working 100%

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,14 +70,17 @@ const questionEl = document.getElementById('question-title');
 const startScreen = document.getElementById('start-screen');
 const optionsEl = document.getElementById('options');
 const checkEl = document.getElementById('check');
-// const optionBtn = document.getElementById('optionBtn');
+const initialsEl = document.getElementById('initials');
+const submitBtn = document.getElementById('submit');
 
 // GIVEN I am taking a code quiz:
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
 
-  // startGame event listener
-startBtn.addEventListener('click', startGame);
+// Event Listener to start game
+if(startBtn) {
+  startBtn.addEventListener('click', startGame);
+}
 
   // Start Game Function
 function startGame() {
@@ -93,6 +96,7 @@ function startGame() {
 
   // set question index to 0
   currentQuestionIndex = 0;
+
   // Math.random to shuffle question array
   shuffler = questions.sort(() => Math.random() - .5);
 
@@ -129,8 +133,15 @@ function currentQuestion(title) {
   })
 }
 
-// Function to check for player guess action
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+  // Function to check for player guess action
 function guess() {
+
+  // Wrong answers deduct time by 15 seconds
   if (this.value !== questions[currentQuestionIndex].answer) {
     totalTime -=15;
 
@@ -138,20 +149,24 @@ function guess() {
      totalTime = 0;
     }
 
+    // Display new time
     timerEl.textContent = totalTime;
 
+    // Wrong answer response
     checkEl.textContent = "Try Again";
   } else {
+    // Right answer response
     checkEl.textContent = "Nice!";
   }
 
-  // flash right/wrong check on page for half a second
   checkEl.setAttribute("class", "check");
   setTimeout(function() {
     checkEl.setAttribute("class", "check hide");
   }, 1000);
 
+  // move to next question
   currentQuestionIndex++;
+
   // Next question in array
   if (currentQuestionIndex === questions.length) {
     gameOver();
@@ -159,7 +174,8 @@ function guess() {
     nextQuestion();
   }
 }
-// Countdown timer function
+
+  // Countdown timer function
 function countdown() {
   // Add decrement to total time for countdown
   totalTime--;
@@ -171,30 +187,69 @@ function countdown() {
   }
 }
 
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+  // Game over function
 gameOver = () => {
+  
+  // stops timer
   clearInterval(timer);
 
+  // hide question card
   questionCardEl.classList.add('hidden');
 
+  // display results page
   const resultsEl = document.getElementById('results');
   resultsEl.classList.remove('hidden');
 
+  // display user score
   const userScore = document.getElementById('userScore');
   userScore.textContent = totalTime;
 }
 
-
-  
-  
-
-
-
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
-//
+
+  // Submit Btn saves score
+  if (submitBtn) {
+    submitBtn.onclick = saveScore;
+  }
+
+  // Function to save player score
+function saveScore() {
+  let initials = initialsEl.value.trim();
+
+  // check for empty value
+  if (initials !== "") {
+    // get scores from local storage, else put new scores in array
+    let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // Create new scoresheet for current user
+    let playerScore = {
+      score: totalTime,
+      initials: initials
+    };
+
+    // save to local storage
+    highscores.push(playerScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    window.location.href = "highscores.html";
+  }
+}
+
+// Check for input by Enter key
+if (initialsEl) {
+  initialsEl.onkeyup = enterCheck;
+}
+
+function enterCheck(event) {
+  if (event.key === 13) {
+    saveScore();
+    console.log('Enter pressed');
+  }
+}
+
+
+
